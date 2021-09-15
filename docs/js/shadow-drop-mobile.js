87dcafcb4661fadff8f3fa5b611f7c769dc87e82
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-09-15 14:33:18 +0100

    Adjusted the scroll in the slider-timer + mobile js with some formatting

--- docs/js/shadow-drop-mobile.js
+++ docs/js/shadow-drop-mobile.js
@@ -7,23 +7,27 @@
 if ($(window).width() > 992) {
   $(window).on("scroll", function() {
    var lScroll = ($(this).scrollTop() / $(window).height())*90 - 100;
-   var tScroll = ($(this).scrollTop() / $(window).height())*80 - 75;
+   var tScroll = ($(this).scrollTop() / $(window).height())*90 - 75;
+   var cScroll = ($(this).scrollTop() / $(window).height())*80 - 75;
    $('.net-zero-container').css({
      'transform': 'translate( 25%,' + (lScroll) + '%)'
    });
    $('.slider-timer').css({
-     'transform': 'translate( 0%,' + (lScroll) + '%)'
+     'transform': 'translate( 0%,' + (tScroll) + '%)'
    })
    $('.mobile-carousel-caption').css({
-     'transform': 'translate( 0%,' + (tScroll) + '%)'
+     'transform': 'translate( 0%,' + (cScroll) + '%)'
    });
  });
 } else {
   $('.net-zero-container').css({
-    'transform': 'translate( 0% 0%)'
+    'transform': 'translate(0% 0%)'
+  });
+  $('.slider-timer').css({
+    'transform': 'translate(0% 0%)'
   });
   $('.mobile-carousel-caption').css({
-    'transform': 'translate( 0% 0%)'
+    'transform': 'translate(0% 0%)'
   });
 }
 
