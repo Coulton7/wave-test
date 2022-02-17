4fc083b32d8057aa0d1afd10dda014834c8f6d44
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-02-17 09:16:12 +0000

    Setting up the new envirologo js

---
+++ docs/js/enviro-logo.js
@@ -0,0 +1,28 @@
+(function($) {
+
+  $.fn.enviroLogo = function() {
+
+    var enviro_logo = jQuery(".enviro_logo");
+    var enviroLogo = -1;
+    var runCount = 0;
+
+    function showNextIcon() {
+      if (runCount < 101) {
+        ++enviroLogo;
+        enviro_logo.eq(enviroLogo % enviro_logo.length)
+          .show(0)
+          .delay(1800)
+          .hide(0, showNextLogo);
+        runCount++;
+      } else {
+        enviro_logo.eq(enviroLogo % enviro_logo.length)
+          .show(0);
+      }
+    }
+    showNextLogo();
+  };
+})(jQuery);
+
+jQuery(document).ready(function() {
+  jQuery('.envirologo-container').enviroLogo();
+});
