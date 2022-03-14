b890dfa46fe658f8c5545a0f735d2e595309c72c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-14 12:56:47 +0000

    Formatted the script and added the new front-fade js

--- docs/js/fade-text.js
+++ docs/js/fade-text.js
@@ -2,22 +2,36 @@
 
   $.fn.fadeText = function(){
 
-    var fade_text = jQuery(".fade_text");
+    var fade_text = $(".fade_text");
     var quoteIndex = -1;
 
     function showNextQuote() {
-        ++quoteIndex;
-        fade_text.eq(quoteIndex % fade_text.length)
-            .fadeIn(900)
-            .delay(900)
-            .fadeOut(900, showNextQuote);
+      ++quoteIndex;
+      fade_text.eq(quoteIndex % fade_text.length)
+      .fadeIn(900)
+      .delay(900)
+      .fadeOut(900, showNextQuote);
     }
 
     showNextQuote();
   };
 
-})(jQuery);
+  $.fn.frontFadeText = function(){
+    var front_fade_text = $('.front-fade');
+    var fadeIndex = -1;
 
-jQuery(document).ready(function(){
-  jQuery('.preface').fadeText();
-});
+    function showNextText() {
+      ++fadeIndex;
+      front_fade_text.eq(fadeIndex % front_fade_text.length)
+      .fadeIn(900)
+      .delay(900)
+      .fadeOut(900, showNextText);
+    }
+    showNextText();
+  };
+
+  $(document).ready(function(){
+    $('.preface').fadeText();
+    $('wavelower').frontFadeText();
+  });
+})(jQuery);
