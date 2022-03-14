6d1b86488baad80a794dce197b05ad11b6baf9e7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-14 16:14:06 +0000

    Removing commented out js to reove an confusion recover later

--- docs/js/shadow-drop-mobile.js
+++ docs/js/shadow-drop-mobile.js
@@ -4,31 +4,4 @@
     $(".dropdown-menu.shadow-drop-menu:first-child").val($(this).text());
  });
 
-/**if ($(window).width() > 992) {
-  $(window).on("scroll load", function() {
-   var lScroll = ($(this).scrollTop() / $(window).height())*90 - 100;
-   var tScroll = ($(this).scrollTop() / $(window).height())*90 - 75;
-   var cScroll = ($(this).scrollTop() / $(window).height())*80 - 75;
-   $('.net-zero-container').css({
-     'transform': 'translate( 25%,' + (lScroll) + '%)'
-   });
-   $('.slider-timer').css({
-     'transform': 'translate( 0%,' + (tScroll) + '%)'
-   })
-   $('.mobile-carousel-caption').css({
-     'transform': 'translate( 0%,' + (cScroll) + '%)'
-   });
- });
-} else {
-  $('.net-zero-container').css({
-    'transform': 'translate(0% 0%)'
-  });
-  $('.slider-timer').css({
-    'transform': 'translate(0% 0%)'
-  });
-  $('.mobile-carousel-caption').css({
-    'transform': 'translate(0% 0%)'
-  });
-}**/
-
 })(jQuery);
