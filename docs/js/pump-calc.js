ed91232d22ab13fdbbd3bea22aaa6676ebb93951
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 09:43:42 +0000

    New html and js to test for calculation lip seal costs see if it prints

---
+++ docs/js/pump-calc.js
@@ -0,0 +1,16 @@
+(function($) {
+  function calculate() {
+    var pumps = parseInt(document.getElementById('pumps').value),
+        cost = 294.06,
+        costYear = pump * cost,
+        labtectaOneYear = 132.06,
+        oneYear = pump * labtectaOneYear,
+        fiveYear = labtectaOneYear + (costYear * 4);
+
+      document.getElementById('lipSealCost').innerHTML = costYear;
+      document.getElementById('yearOne').innerHTML = oneYear;
+      document.getElementById('annualYear').innerHTML = costYear;
+      document.getElementById('fiveYear').innerHTML = fiveYear;
+
+  }
+});
