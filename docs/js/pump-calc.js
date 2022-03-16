b08d1e29f54600d792e514ad282bfb5620e52c55
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 12:27:48 +0000

    Changing all of htme to the locale string as it appears to work

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -7,9 +7,9 @@
         oneYear = pumps * labtectaOneYear,
         fiveYear = (pumps * labtectaOneYear) + (pumps * cost * 4);
 
-      document.getElementById('lipSealCost').innerHTML = '£' + costYear.toLocaleString(undefined, {minimumFractionDigits: 2});
-      document.getElementById('yearOne').innerHTML = '£' + oneYear.toLocaleString(undefined, {minimumFractionDigits: 2});
-      document.getElementById('annualYear').innerHTML = '£' + costYear.toLocaleString(undefined, {minimumFractionDigits: 2});
+      document.getElementById('lipSealCost').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+      document.getElementById('yearOne').innerHTML = oneYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+      document.getElementById('annualYear').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
       document.getElementById('fiveYear').innerHTML = fiveYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
   }
 
