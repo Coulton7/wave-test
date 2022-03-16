174ee5829159a0125892012319d7737f85385da0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 11:45:00 +0000

    Adjusting the to locale string

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -7,10 +7,10 @@
         oneYear = pumps * labtectaOneYear,
         fiveYear = (pumps * labtectaOneYear) + (pumps * cost * 4);
 
-      document.getElementById('lipSealCost').innerHTML = '£' + costYear.toLocaleString(2);
-      document.getElementById('yearOne').innerHTML = '£' + oneYear.toLocaleString(2);
-      document.getElementById('annualYear').innerHTML = '£' + costYear.toLocaleString(2);
-      document.getElementById('fiveYear').innerHTML = '£' + fiveYear.toLocaleString(2);
+      document.getElementById('lipSealCost').innerHTML = '£' + costYear.toLocaleString({minimumFractionDigits: 2});
+      document.getElementById('yearOne').innerHTML = '£' + oneYear.toLocaleString({minimumFractionDigits: 2});
+      document.getElementById('annualYear').innerHTML = '£' + costYear.toLocaleString({minimumFractionDigits: 2});
+      document.getElementById('fiveYear').innerHTML = '£' + fiveYear.toLocaleString({minimumFractionDigits: 2});
   }
 
   $(document).ready(function() {
