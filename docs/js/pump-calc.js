c2b7777754a10e12429491250195f36d05ce9262
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 10:24:49 +0000

    Small adjustment to 2 decimal places

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -2,10 +2,10 @@
   function calculate() {
     var pumps = document.getElementById('pumps').value,
         cost = 294.06,
-        costYear = pumps * cost,
+        costYear = pumps * cost.toFixed(2),
         labtectaOneYear = 132.06,
-        oneYear = pumps * labtectaOneYear,
-        fiveYear = labtectaOneYear + (costYear * 4);
+        oneYear = pumps * labtectaOneYear.toFixed(2),
+        fiveYear = labtectaOneYear + (costYear * 4).toFixed(2);
 
       document.getElementById('lipSealCost').innerHTML = costYear;
       document.getElementById('yearOne').innerHTML = oneYear;
