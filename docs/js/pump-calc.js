00d459b7a04c36f22a3015d5f7c2e481555a3f6c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 09:51:16 +0000

    undefined variable will have prevented js from working

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -2,9 +2,9 @@
   function calculate() {
     var pumps = parseInt(document.getElementById('pumps').value),
         cost = 294.06,
-        costYear = pump * cost,
+        costYear = pumps * cost,
         labtectaOneYear = 132.06,
-        oneYear = pump * labtectaOneYear,
+        oneYear = pumps * labtectaOneYear,
         fiveYear = labtectaOneYear + (costYear * 4);
 
       document.getElementById('lipSealCost').innerHTML = costYear;
