4d085eb5e6d879e4ae6b70f52f358bd5f1829c68
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-16 10:28:53 +0000

    to decimal places not worked but £ did

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -2,10 +2,10 @@
   function calculate() {
     var pumps = document.getElementById('pumps').value,
         cost = 294.06,
-        costYear = pumps * cost.toFixed(2),
+        costYear = pumps * cost,
         labtectaOneYear = 132.06,
-        oneYear = pumps * labtectaOneYear.toFixed(2),
-        fiveYear = labtectaOneYear + (costYear * 4).toFixed(2);
+        oneYear = pumps * labtectaOneYear,
+        fiveYear = labtectaOneYear + (costYear * 4);
 
       document.getElementById('lipSealCost').innerHTML = costYear;
       document.getElementById('yearOne').innerHTML = oneYear;
