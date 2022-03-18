6f905b2514bb81329b3e2107cf3e430cc6eab801
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-03-18 14:16:33 +0000

    Testing the new power consumption script

--- docs/js/pump-calc.js
+++ docs/js/pump-calc.js
@@ -1,15 +1,15 @@
-(function($) {
+(f(function($) {
   function calculate() {
     var pumps = document.getElementById('pumps').value,
         cost = 294.06,
         costYear = pumps * cost,
-        labtectaOneYear = 132.06,
-        oneYear = pumps * labtectaOneYear,
-        fiveYear = (pumps * labtectaOneYear) + (pumps * cost * 4);
+        fiveYear = cost * 5,
+        powerCon = 2803,
+        powerConY= powerCon * pumps;
 
       document.getElementById('lipSealCost').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
-      document.getElementById('yearOne').innerHTML = oneYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
       document.getElementById('annualYear').innerHTML = costYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
+      document.getElementById('powerConsumption').innerHTML = powerConY.toLocaleString("en-GB");
       document.getElementById('fiveYear').innerHTML = fiveYear.toLocaleString("en-GB", {style:"currency", currency:"gbp"});
   }
 
