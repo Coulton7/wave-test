ab05617e8acb852d5ac682b93918701e709eb440
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-04-07 14:07:54 +0100

    Set the reset up for it

--- docs/js/home-carousel-hider.js
+++ docs/js/home-carousel-hider.js
@@ -5,7 +5,6 @@
       var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
       var total = carouselData.$items.length;
       var slide = (currentIndex + 1);
-      reset();
       if (slide === 1) {
         if ($('.sus-media-carousel').hasClass('display-none')) {
           $('.sus-media-carousel').removeClass('display-none');
