9525bc3fc2a69eb73365d179ae2a23a972b153b3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-04-12 14:59:02 +0100

    Changing the url slightly for the test

--- docs/js/get-url.js
+++ docs/js/get-url.js
@@ -14,12 +14,12 @@
       return vars;
     }
 
-    var salesString = getUrlVars()["sc"];
+    var salesString = getUrlVars()["s"];
     if(salesString=='ml'){
-      document.getElementById('sales-ml').style.display = 'block';
+      document.getElementById('m').style.display = 'block';
     }
-    else if (salesString=='li') {
-      document.getElementById('sales-li').style.display = 'block';
+    else if (salesString=='l') {
+      document.getElementById('l').style.display = 'block';
     }
   });
 })(jQuery);
