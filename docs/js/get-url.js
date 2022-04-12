a16f97a08ab62c8a4a5852249b8c72c887a7ac43
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-04-12 13:51:49 +0100

    Adding new js file for testing not related to the homepage slider

---
+++ docs/js/get-url.js
@@ -0,0 +1,25 @@
+(function($){
+  $(document).ready(function() {
+
+    function getUrlVars()
+    {
+      var vars = [], hash;
+      var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+      for(var i = 0; i < hashes.length; i++)
+      {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+      }
+      return vars;
+    }
+
+    var salesString = getUrlVars()["sc"];
+    if(salesString=='ml'){
+      $('#sales-ml').show();
+    }
+    else if (submitString='li') {
+      $('#sales-li').show();
+    }
+  });
+});
