436f9ed8e94dec8b4ba7a645ed1288d21ddd1dae
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-04-28 11:09:42 +0100

    adjusting js in the wrong order

--- docs/js/carousel-options.js
+++ docs/js/carousel-options.js
@@ -213,11 +213,11 @@ var isOnDiv = false;
 
     for (let x=0; x<mediaCarousel.length; x++) {
       mediaCarousel[x].addEventListener("mouseleave", function(){
-        $(".nav-help").fadeIn(1800);
+        $(".nav-help").fadeOut(1800);
       });
 
       mediaCarousel[x].addEventListener("mouseenter", function(){
-        $(".nav-help").fadeOut(1800);
+        $(".nav-help").fadein(1800);
       });
     }
 
