43017426a3ba8a233d63f973b9c5068ca2048ed4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-12 10:44:21 +0100

    Upping the fadeto slightly to test the effect

--- docs/js/carousel-options.js
+++ docs/js/carousel-options.js
@@ -217,7 +217,7 @@ var isOnDiv = false;
         });
 
         mediaCarousel[x].addEventListener("mouseenter", function(){
-          $(".nav-help").fadeTo(1000, 1).css('visibility', 'visible');
+          $(".nav-help").fadeTo(2000, 1).css('visibility', 'visible');
         });
       }
     }
