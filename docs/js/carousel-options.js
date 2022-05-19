24eb3b15581458b8cb11b4a9263d09afe3ecb951
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-05-19 16:32:53 +0100

    Reset internestingly despite position absolutenbeing in affect

--- docs/js/carousel-options.js
+++ docs/js/carousel-options.js
@@ -213,11 +213,11 @@ var isOnDiv = false;
     if ($(window).width() > 991) {
       for (let x=0; x<mediaCarousel.length; x++) {
         mediaCarousel[x].addEventListener("mouseleave", function(){
-          $(".nav-help").hide();
+          $(".nav-help").css('visibility', 'hidden').fadeTo(1000, 0);
         });
 
         mediaCarousel[x].addEventListener("mouseenter", function(){
-          $(".nav-help").fadeIn(1000);
+          $(".nav-help").css('visibility', 'visible').fadeTo(1000, 1);
         });
       }
     }
