0b46d24f4dec65396304cee195299b7c98f7b9cc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-03-03 14:01:20 +0000

    Trying with a small adjustment

--- docs/js/owl-carousel.js
+++ docs/js/owl-carousel.js
@@ -5,7 +5,7 @@ $(function(){
         stagePadding: 50,
         items: 4,
         nav: true,
-        navText:["<div class='carousel-controls'><i class='fas fas fa-angle-left fa-3x'></i>","<i class='fas fas fa-angle-right fa-3x'></i></div>"],
+        navText:["<div class='carousel-controls'><i class='fas fas fa-angle-left fa-3x'></i> <i class='fas fas fa-angle-right fa-3x'></i></div>"],
         responsive: {
             767 : {
              items: 4   
