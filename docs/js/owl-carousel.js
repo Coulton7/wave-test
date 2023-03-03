784d95c3efba8df0fb6650ad7ada62898bac28e4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-03-03 10:01:34 +0000

    Adding the base js for the new carousel slider

---
+++ docs/js/owl-carousel.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('.thumb-carousel').owlCarousel({
+        loop: true,
+        stagePadding: 50,
+        items: 4,
+        nav: true,
+        responsive: {
+            767 : {
+             items: 4   
+            },
+
+            768: {
+                items: 3
+            },
+
+            992: {
+                itmes: 4
+            },
+
+            1200: {
+                items: 4
+            }
+        }
+    })
+})
