88c18c2a4bd1cefcd5561ecb1d711bba5bd074c5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-03-03 13:59:01 +0000

    Changing the carousel-controls

--- docs/js/owl-carousel.js
+++ docs/js/owl-carousel.js
@@ -5,7 +5,7 @@ $(function(){
         stagePadding: 50,
         items: 4,
         nav: true,
-        navText:["<i class='fas fas fa-angle-left fa-3x'></i>","<i class='fas fas fa-angle-right fa-3x'></i>"],
+        navText:["<div class='carousel-controls'><i class='fas fas fa-angle-left fa-3x'></i>","<i class='fas fas fa-angle-right fa-3x'></i></div>"],
         responsive: {
             767 : {
              items: 4   
