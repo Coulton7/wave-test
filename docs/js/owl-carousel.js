59269e8d9a71c257645ab3dfd25912564ab11f03
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-03-09 11:11:06 +0000

    Dropping pane count down to two

--- docs/js/owl-carousel.js
+++ docs/js/owl-carousel.js
@@ -15,7 +15,7 @@ $(function($){
         navText:["<i class='fas fa-caret-left fa-3x'></i>","<i class='fas fa-caret-right fa-3x'></i>"],
         responsive: {
             767 : {
-             items: 4   
+             items: 2   
             },
     
             768: {
