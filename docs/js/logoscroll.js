1200f42b5cae4f27ee84819acb0f56b542f628a5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-03-21 16:03:02 +0000

    Dropping to 0.37 as a test

--- docs/js/logoscroll.js
+++ docs/js/logoscroll.js
@@ -61,12 +61,12 @@ $(window).width(function(e) {
         $('.social-media li a').removeClass('normText');
       }
 
-      if (opacity >= 0.41) {
+      if (opacity >= 0.37) {
         $('.carousel-caption h1').addClass('transparent-text');
         $('.carousel-caption h4').addClass('transparent-text');
       }
 
-      else if (opacity < 0.41) {
+      else if (opacity < 0.37) {
         $('.carousel-caption h1').removeClass('transparent-text');
         $('.carousel-caption h4').removeClass('transparent-text');
       }
