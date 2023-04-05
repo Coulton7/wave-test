3913cfbdec9fae697f6d8f017c3aae93191db988
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-04-05 16:01:52 +0100

    Added the js for the tablet

--- docs/js/nav-btn.js
+++ docs/js/nav-btn.js
@@ -33,6 +33,37 @@
           }
       });
 
+      $("#tablet-overlay-btn").click(
+        function navBtn() {
+          document.getElementById('tablet-overlay-btn').classList.toggle("change");
+          if (x.style.height === "100%") {
+              x.style.height = "0%";
+              $('.main').removeClass('active');
+              $("body").removeClass('noscroll');
+              $('#overlay-btn').addClass('overlay-btn');
+              $('.search-block').removeClass('overlay-search');
+              $('.navigation_col').removeClass('overlay-menu');
+              $(y).addClass('fixed-top');
+            } else {
+              x.style.height = "100%";
+              if($('#prodForm').hasClass('in')) {
+                $('#prodForm').modal('hide')
+              }
+              if($('#resForm').hasClass('in')) {
+                $('#resForm').modal('hide')
+              }
+              if($('#location-details').dialog('isOpen') === true) {
+                $('#location-details').dialog('close');
+              }
+              $('.main').addClass('active');
+              $("body").addClass('noscroll');
+              $('#overlay-btn').removeClass('overlay-btn');
+              $('.search-block').addClass('overlay-search');
+              $('.navigation_col').addClass('overlay-menu');
+              $(y).removeClass('fixed-top');
+            }
+        });
+
       $("#footer-overlay-btn").click(
         function footerNavbtn(){
           document.getElementById('footer-overlay-btn').classList.toggle("change");
