d8def337dad9c00eff31174da297401a78a57bb3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-04 10:47:12 +0100

    Updating js for the menu collapse to be all vanilla

--- docs/js/menu-collapse.js
+++ docs/js/menu-collapse.js
@@ -1,61 +1,59 @@
-
-  function autocollapse() {
-    var navbar = document.querySelector('#autocollapse');
+function autocollapse() {
+  var navbar = document.getElementById('autocollapse');
+  navbar.classList.remove('collapsed');
+  if(navbar.innerHeight() > 100) {
+    navbar.classList.add('collapsed');
+    document.getElementById('overlay-btn').classList.add('display-none-important');
+  } else {
     navbar.classList.remove('collapsed');
-    if(navbar.innerHeight() > 100) {
-      navbar.classList.add('collapsed');
-      document.querySelector('#overlay-btn').classList.add('display-none-important');
-    } else {
-      navbar.classList.remove('collapsed');
-      $('#overlay-btn').classList.remove('display-none-important');
+    document.getElementById('overlay-btn').classList.remove('display-none-important');
+  }
+}
+
+function menuItemOverlay() {
+
+  if(window.innerWidth > 992) {
+    let overlayBtn = document.querySelector('.overlay-btn').getBoundingClientRect();
+    let overlayBtnLeft = overlayBtn.left;
+    let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
+    let menuItemRight = menuItem.right;
+
+    if ((menuItemRight > overlayBtnLeft)) {
+      document.getElementById('overlay-btn').classList.add('display-none-important');
+    }
+    else {
+      document.getElementById('overlay-btn').classList.remove('display-none-important');
     }
   }
 
-  function menuItemOverlay() {
+  if(window.innerWidth >= 768 && window.innerWidth <= 991) {
+    let tabletBtn = document.querySelector('.tablet-overlay-btn').getBoundingClientRect();
+    let tabletBtnLeft = tabletBtn.left;
+    let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
+    let menuItemRight = menuItem.right;
 
-    if(window.innerWidth > 992) {
-      let overlayBtn = document.querySelector('.overlay-btn').getBoundingClientRect();
-      let overlayBtnLeft = overlayBtn.left;
-      let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
-      let menuItemRight = menuItem.right;
-
-      if ((menuItemRight > overlayBtnLeft)) {
-        $('#overlay-btn').addClass('display-none-important');
-      }
-      else {
-        $('#overlay-btn').removeClass('display-none-important');
-      }
+    if ((menuItemRight > tabletBtnLeft)) {
+      document.querySelector('.tablet-overlay-btn').classList.add('display-none-important');
     }
-
-    if(window.innerWidth >= 768 && window.innerWidth <= 991) {
-      let tabletBtn = document.querySelector('.tablet-overlay-btn').getBoundingClientRect();
-      let tabletBtnLeft = tabletBtn.left;
-      let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
-      let menuItemRight = menuItem.right;
-
-      if ((menuItemRight > tabletBtnLeft)) {
-        document.querySelector('.tablet-overlay-btn').classList.add('display-none-important');
-      }
-      else {
-        document.querySelector('.tablet-overlay-btn').classList.remove('display-none-important');
-      }
-
+    else {
+      document.querySelector('.tablet-overlay-btn').classList.remove('display-none-important');
     }
   }
+}
 
-  document.addEventListener("DOMContentLoaded", autocollapse);
-  document.addEventListener("DOMContentLoaded", menuItemOverlay);
-  window.addEventListener('resize', function() {
-    if($("#overlay-btn").hasClass("change")) {
-      return;
-    } else {
-      autocollapse();
-    }
-  });
-  window.addEventListener('resize', function() {
-    if(this.document.querySelector("#overlay-btn").classList.contains("change")) {
-      return;
-    } else {
-      menuItemOverlay();
-    }
-  });
+document.addEventListener("DOMContentLoaded", autocollapse);
+document.addEventListener("DOMContentLoaded", menuItemOverlay);
+window.addEventListener('resize', function() {
+  if(document.getElementById("#overlay-btn").classList.contains("change")) {
+    return;
+  } else {
+    autocollapse();
+  }
+});
+window.addEventListener('resize', function() {
+  if(this.document.getElementById("overlay-btn").classList.contains("change")) {
+    return;
+  } else {
+    menuItemOverlay();
+  }
+});
