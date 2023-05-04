c37b7fd84d759be7914cdec242378d310c21b113
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-04 11:03:30 +0100

    Small adjustment to the js

--- docs/js/shadow-drop-mobile.js
+++ docs/js/shadow-drop-mobile.js
@@ -1,6 +1,8 @@
-var dropdown = document.querySelector(".dropdown-menu li a");
+window.addEventListener("DOMContentLoaded", (event) => {
+  var dropdown = document.querySelector(".dropdown-menu li a");
 
-dropdown.addEventListener('click', function(){
-  document.querySelector(this).parents(".dropdown").find('.btn').html(document.querySelector(this).text() + ' <span class="caret"></span>');
-  document.querySelector(this).parents(".dropdown").find('.btn').val(document.querySelector(this).data('value'));
+  dropdown.addEventListener('click', function(){
+    document.querySelector(this).parents(".dropdown").find('.btn').html(document.querySelector(this).text() + ' <span class="caret"></span>');
+    document.querySelector(this).parents(".dropdown").find('.btn').val(document.querySelector(this).data('value'));
+  })
 });
