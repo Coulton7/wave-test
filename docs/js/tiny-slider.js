a026f4b7afdbc2fd8ab109b6d3b88cd668f381f9
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-04 10:13:38 +0100

    tiny-slider jsNew css and js for tiny-slider

---
+++ docs/js/tiny-slider.js
@@ -0,0 +1,36 @@
+var slider = tns({
+    container: ".slider",
+    slideBy: 1,
+    loop: true,
+    nav: false,
+    controls: true,
+    controlsContainer: "arrow-controls",
+    controlsPosition: "bottom",
+    responsive: {
+        1 : {
+        items: 2 
+        },
+
+        600 : {
+        items: 3
+        },
+
+        768: {
+            items: 3.66
+        },
+
+        992: {
+            itmes: 4.33
+        },
+
+        1200: {
+            items: 4.33
+        }
+    },
+    lazyload: true,
+})
+
+slider.events.on("translationEnd", data => {
+    let {controlsContainer, displayIndex, slideCount} = data;
+    document.getElementById("counter").textContent('${displayIndex} of {$slideCount}')
+})
