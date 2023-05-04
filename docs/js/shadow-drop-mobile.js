53591f877220c9c247bf835856f4e2bedb2482c2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-04 10:58:26 +0100

    Updating the file

--- docs/js/shadow-drop-mobile.js
+++ docs/js/shadow-drop-mobile.js
@@ -1,4 +1,6 @@
-document.querySelector(".dropdown-menu li a").addEventListener('click', function(){
+var dropdown = document.querySelector(".dropdown-menu li a");
+
+dropdown.addEventListener('click', function(){
   document.querySelector(this).parents(".dropdown").find('.btn').html(document.querySelector(this).text() + ' <span class="caret"></span>');
   document.querySelector(this).parents(".dropdown").find('.btn').val(document.querySelector(this).data('value'));
 });
