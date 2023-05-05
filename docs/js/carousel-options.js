777a058eb172dd82d57d809b57eaba1dca78b8f5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-05 12:04:13 +0100

    Changing back to all

--- docs/js/carousel-options.js
+++ docs/js/carousel-options.js
@@ -12,7 +12,7 @@ document.querySelectorAll('.media-carousel-block').forEach((i) => {
 });
 
 function carouselNormalization() {
-  var items = document.querySelector('#carousel-fade .item'),
+  var items = document.querySelectorAll('#carousel-fade .item'),
     heights = [],
     tallest;
 
