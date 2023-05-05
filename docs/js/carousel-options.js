75c5e3baa5d655b00c5adc373bad1daf64569f49
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-05 13:09:22 +0100

    Changing to resize rule

--- docs/js/carousel-options.js
+++ docs/js/carousel-options.js
@@ -26,7 +26,7 @@ function carouselNormalization() {
     }
     normalizeHeights();
 
-    window.addEventListener('resize orientationchange', function () {
+    window.addEventListener('resize', function () {
       window.tallest = 0, widnow.heights.length = 0;
       document.querySelectorAll('#carousel-fade .carousel-inner .carousel-item').forEach(function () {
         document.querySelector('#carousel-fade .carousel-inner .carousel-item').style.minHeight = '0';
