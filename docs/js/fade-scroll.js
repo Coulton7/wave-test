f7bf72c43cebe42f27df9874fa6e1353fe64710b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 11:15:05 +0100

    Moving some of the items around

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -1,10 +1,10 @@
-var textLogo = document.getElementById('textlogo')
-
 document.addEventListener("DOMContentLoaded", function() {
+    var textLogo = document.getElementById('#textlogo')
     textLogo.style.opacity = -1.03;
-});
 
-window.addEventListener('scroll', function(e) {
-    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    textLogo.style.opacity = Math.max(-1, Math.min(-1.03, + scroll * 4));
+    window.addEventListener('scroll', function(e) {
+        var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        textLogo.style.opacity = Math.max(-1, Math.min(-1.03, + scroll * 4));
+    });
+
 });
