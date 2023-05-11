785e622210a9b39fd91dc5eb80c3e05b5b99f78a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 14:35:58 +0100

    Adding foreach to the statements

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -2,26 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const fadeScroll = document.querySelectorAll('.fade-scroll')
 
     if(window.innerWidth > 992) {
-    fadeScroll.style.opacity = -1.03;
+    fadeScroll.forEach.style.opacity = -1.03;
 
     window.addEventListener('scroll', function(e) {
         var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        fadeScroll.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+        fadeScroll.forEach.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
     });
     } else {
-        fadeScroll.style.opacity = 1;
+        fadeScroll.forEach.style.opacity = 1;
     }
 
     window.addEventListener('resize', function(){
         if(window.innerWidth > 992) {
-            fadeScroll.style.opacity = -1.03;
+            fadeScroll.forEach.style.opacity = -1.03;
         
             window.addEventListener('scroll', function(e) {
                 var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-                fadeScroll.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+                fadeScroll.forEach.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
             });
         } else {
-            fadeScroll.style.opacity = 1;
+            fadeScroll.forEach.style.opacity = 1;
         }
     });
 });
