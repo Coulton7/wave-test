b2e429338efcad950e313dbbb81ff8a848b04fd5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 11:45:31 +0100

    Adding the width checking class for test effect

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     var textLogo = document.getElementById('textlogo')
+
+    if(window.innerWidth > 992) {
     textLogo.style.opacity = -1.03;
 
     window.addEventListener('scroll', function(e) {
         var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 400));
     });
+    } else {
+        textLogo.style.opacity = 1;
+    }
 
 });
