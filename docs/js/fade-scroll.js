0425c009cac9c80d5361d165a06b8680aa615321
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 16:32:36 +0100

    Adding a new one for the background of the navbar-nav

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -2,22 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const textLogo = document.getElementById('textlogo');
     const sectionShadow = document.getElementById('section-shadow');
     const enquiry = document.getElementById('enq-icon');
+    const navbar = document.querySelector('.navbar-nav')
 
     if(window.innerWidth > 992) {
         sectionShadow.style.opacity = -1.03;
         textLogo.style.opacity = -1.03;
         enquiry.style.opacity = -1.03;
+        navbar.style.backgroundColor = "rgba(255,255,255,0)";
 
     window.addEventListener('scroll', function(e) {
         var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
         textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
         enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+        navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 500)); +")"
     });
     } else {
         sectionShadow.style.opacity = 1;
         textLogo.style.opacity = 1;
         enquiry.style.opacity = 1;
+        navbar.style.backgroundColor = "rgba(255,255,255,1)";
     }
 
     window.addEventListener('resize', function(){
@@ -25,17 +29,20 @@ document.addEventListener("DOMContentLoaded", function() {
             sectionShadow.style.opacity = -1.03;
             textLogo.style.opacity = -1.03;
             enquiry.style.opacity = -1.03;
+            navbar.style.backgroundColor = "rgba(255,255,255,0)";
         
             window.addEventListener('scroll', function(e) {
                 var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
                 sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
                 textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
                 enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+                navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 500)); +")"
             });
         } else {
             sectionShadow.style.opacity = 1;
             textLogo.style.opacity = 1;
             enquiry.style.opacity = 1;
+            navbar.style.backgroundColor = "rgba(255,255,255,1)";
         }
     });
 
