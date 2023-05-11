b60ded8beb9e4f834f4c66f8d289666b99eb5eb1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 11:50:03 +0100

    Adjusted the js for new resizing

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -12,4 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
         textLogo.style.opacity = 1;
     }
 
+    window.addEventListener('resize', function(){
+        if(window.innerWidth > 992) {
+            textLogo.style.opacity = -1.03;
+        
+            window.addEventListener('scroll', function(e) {
+                var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+                textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 400));
+            });
+        } else {
+            textLogo.style.opacity = 1;
+        }
+    });
 });
