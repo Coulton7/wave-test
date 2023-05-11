c48c28ba0ad329b95134449267871362bb4405e4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 11:37:48 +0100

    Trying another adjustment to the scorll

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener('scroll', function(e) {
         var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        textLogo.style.opacity = Math.max(1, Math.min(-1.03, + scroll / 4));
+        textLogo.style.opacity = Math.max(1, Math.min(-1.03, -scroll / 4));
     });
 
 });
