4d8aac5db2620bdb5ce3e2b40b5842eb7d90291a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-11 12:09:34 +0100

    Trying with new fade-scroll class for targeting method

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var textLogo = document.getElementById('textlogo')
+    var fadeScroll = document.querySelectorAll('.fade-scroll')
 
     if(window.innerWidth > 992) {
-    textLogo.style.opacity = -1.03;
+    fadeScroll.style.opacity = -1.03;
 
     window.addEventListener('scroll', function(e) {
         var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+        fadeScroll.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
     });
     } else {
-        textLogo.style.opacity = 1;
+        fadeScroll.style.opacity = 1;
     }
 
     window.addEventListener('resize', function(){
         if(window.innerWidth > 992) {
-            textLogo.style.opacity = -1.03;
+            fadeScroll.style.opacity = -1.03;
         
             window.addEventListener('scroll', function(e) {
                 var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-                textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
+                fadeScroll.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
             });
         } else {
-            textLogo.style.opacity = 1;
+            fadeScroll.style.opacity = 1;
         }
     });
 });
