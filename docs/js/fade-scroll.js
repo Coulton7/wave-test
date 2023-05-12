7a8958c0fb981c0951ceec9a84c4441f02cb5c84
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-05-12 11:04:17 +0100

    Trying another targeting style

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const enquiry = document.getElementById('enq-icon');
     const navbar = document.querySelector('.navbar-nav');
     var navLinks = document.querySelectorAll('.navbar-nav a');
-    var socialLinks = document.querySelectorAll('.social-media li');
+    var socialLinks = document.querySelectorAll('.social-media>li>a');
 
     if(window.innerWidth > 992) {
         sectionShadow.style.opacity = -1.03;
