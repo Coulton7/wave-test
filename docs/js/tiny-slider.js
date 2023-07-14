e0cc0b577649f91d7bb6179a8824cece0ae437a3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-07-14 15:19:08 +0100

    Removing autoWidth causes too much chaos

--- docs/js/tiny-slider.js
+++ docs/js/tiny-slider.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
         controlsContainer: "#arrow-controls",
         gutter: 20,
         lazyload: true,
-        autoWidth: true,
         responsive: {
             1 : {
             items: 2 
