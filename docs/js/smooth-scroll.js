3df5aa6edaf7ad763fac465b112a70a8f1f91ed5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-07-24 16:53:36 +0100

    Adding new smooth scroll js as a test

---
+++ docs/js/smooth-scroll.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('a[href*="#"]');
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const offsetTop = document.querySelector(links).offsetTop;
+}
+
+scroll({
+  top:offsetTop,
+  behavior: "smooth"
+});
