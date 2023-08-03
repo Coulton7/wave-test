5585b57a0a43af270af82e155aad8ba9769f5ea8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-03 14:18:08 +0100

    Small formatting change

--- docs/js/rotate-img.js
+++ docs/js/rotate-img.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function initArray() {
         var img = document.querySelectorAll(".img-bank img");
         img.forEach(function () {
-            var src = img.getAttribute("src")
+            var src = img.getAttribute("src");
             ids.push.src;         
         });
     }
