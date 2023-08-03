f5c1cfbb5b15d9be667d4a7411c99b4388b0733d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-03 11:50:27 +0100

    New grid and first attempt js for changing imgs

---
+++ docs/js/rotate-img.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var ids = []
+
+    function initArray() {
+        document.querySelector(".img-bank img").forEach(function () {
+            ids.push(this.attr("src"));            
+        });
+
+        function randomArray() {
+            var tempIds = ids.slice();
+
+            var myIds = [];
+
+            for( var i = 0; i < 12; i++) {
+                var randomId = (Math.floor(Math.random() * tempIds.length) + 1);
+
+                var myId = tempIds[randomId - 1];
+
+                myIds.push(myId);
+
+                var pos = tempIds.indexOf(myId);
+
+                tempIds.splice(pos, 1);
+            }
+            return myIds;
+        }
+        initArray();
+    }
+
+    function changeSrc() {
+        var result = randomArray();
+        var cells = document.querySelectorAll(".gallery .col-2");
+        var randomId = (Math.floor(Math.random() * cells.length));
+        var cell = cells.eq(randomId);
+
+        cell.find("img:hidden").forEach(function(index){
+            this.attr("src", result[index]);
+        });
+        cells.find("img").fadeToggle(1500);
+    }
+
+    setInterval(function() {
+        changeSrc();
+    }, 2000);
+})
