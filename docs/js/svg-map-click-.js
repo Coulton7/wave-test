b4b139f93cfe791d35a28b58bca9e7e796e3198c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-08 13:07:55 +0100

    Trying with a small variation on the code test

--- docs/js/svg-map-click-.js
+++ docs/js/svg-map-click-.js
@@ -284,12 +284,12 @@ document.addEventListener("DOMContentLoaded", function () {
           break;
       }
   
-      allRegions.classList.remove("on");
+      allRegions[0].classList.remove("on");
       locModal.show();
-      allRegions.classList.add("on");
+      allRegions[0].classList.add("on");
       document.querySelector("body").classList.add("noscroll");
       document.querySelector(".location-pop-up").addEventListener("hidden.bs.modal", event => {
-          allRegions.classList.remove("on");
+          allRegions[0].classList.remove("on");
           document.querySelector("body").classList.remove("noscroll");
   
           switch (true) {
