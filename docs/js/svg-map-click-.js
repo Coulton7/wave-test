d1f704c1276e9be0cec29efb73ab1140fc91ed6e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-09 11:23:06 +0100

    Trying to add changes for the height (hide overlay)

--- docs/js/svg-map-click-.js
+++ docs/js/svg-map-click-.js
@@ -572,8 +572,9 @@ document.addEventListener("DOMContentLoaded", function () {
       x.style.height = "100%";
       document.querySelector("location-overlay").classList.toggle('change');
       document.querySelector("body").classList.add("noscroll");
-      document.querySelector(".location-pop-up").addEventListener("hidden.bs.modal", (event) => {
+      document.querySelector("change").addEventListener("click", function () {
           document.querySelector("body").classList.remove("noscroll");
+          x.style.height = "0%";
 
           switch (true) {
             case link == "Land-Maine":
