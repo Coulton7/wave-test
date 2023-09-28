34d137650eead8408b8f7a88ce6ea83a85ca0587
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-09-28 12:44:45 +0100

    Trying the scroll listener as a function

--- docs/js/fade-scroll.js
+++ docs/js/fade-scroll.js
@@ -6,37 +6,41 @@ document.addEventListener("DOMContentLoaded", function() {
     var navLinks = document.querySelectorAll('.navbar-nav a');
     var socialLinks = document.querySelectorAll('.social-media a');
 
+    function fadeScroll() {
+        window.addEventListener('scroll', function(e) {
+            var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
+    
+            if(scroll/600 >= 0.75) {
+                for (var i = 0; i < navLinks.length; i++) {
+                    navLinks[i].classList.add('normText');
+                }
+                for (var y = 0; y < socialLinks.length; y++) {
+                    socialLinks[y].classList.add('normText');
+                }
+            }
+    
+            if(scroll/600 < 0.75) {
+                for (var i = 0; i < navLinks.length; i++) {
+                    navLinks[i].classList.remove('normText');
+                }
+                for (var y = 0; y < socialLinks.length; y++) {
+                    socialLinks[y].classList.remove('normText');
+                }
+            }
+        });
+    };
+
     if(window.innerWidth > 992) {
         sectionShadow.style.opacity = -1.03;
         textLogo.style.opacity = -1.03;
         enquiry.style.opacity = -1.03;
         navbar.style.backgroundColor = "rgba(255,255,255,0)";
+        fadeScroll();
 
-    window.addEventListener('scroll', function(e) {
-        var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
-
-        if(scroll/600 >= 0.75) {
-            for (var i = 0; i < navLinks.length; i++) {
-                navLinks[i].classList.add('normText');
-            }
-            for (var y = 0; y < socialLinks.length; y++) {
-                socialLinks[y].classList.add('normText');
-            }
-        }
-
-        if(scroll/600 < 0.75) {
-            for (var i = 0; i < navLinks.length; i++) {
-                navLinks[i].classList.remove('normText');
-            }
-            for (var y = 0; y < socialLinks.length; y++) {
-                socialLinks[y].classList.remove('normText');
-            }
-        }
-    });
     } else {
         sectionShadow.style.opacity = 1;
         textLogo.style.opacity = 1;
@@ -50,24 +54,8 @@ document.addEventListener("DOMContentLoaded", function() {
             textLogo.style.opacity = -1.03;
             enquiry.style.opacity = -1.03;
             navbar.style.backgroundColor = "rgba(255,255,255,0)";
+            fadeScroll();
         
-            window.addEventListener('scroll', function(e) {
-                var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-                sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-                textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-                enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
-                navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
-
-                if(scroll/600 >= 0.75) {
-                    document.querySelector('.navbar-nav a').classList.add('normText');
-                    document.querySelector('.social-media a').classList.add('normText');
-                }
-        
-                if(scroll/600 < 0.75) {
-                    document.querySelector('.navbar-nav a').classList.remove('normText');
-                    document.querySelector('.social-media a').classList.remove('normText');
-                }
-            });
         } else {
             sectionShadow.style.opacity = 1;
             textLogo.style.opacity = 1;
