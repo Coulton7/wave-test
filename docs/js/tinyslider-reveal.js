b7ac69a36c510057ec722324c4adb34a0168623b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-02-21 11:44:15 +0000

    Changing the btn and included visibility to stop jumping

--- docs/js/tinyslider-reveal.js
+++ docs/js/tinyslider-reveal.js
@@ -4,8 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("#newsTab").classList.add('d-none');
         document.querySelector("#pillarTab").classList.remove('d-none');
         document.querySelector("#pillarTab").classList.add('d-block');
-        if(document.querySelector("#newsBtn").classList.contains('d-none')){
-            document.querySelector("#newsBtn").classList.remove('d-none');
+        if(document.querySelector("#newsBtn").classList.contains('invisible')){
+            document.querySelector("#newsBtn").classList.remove('invisible');
+            document.querySelector("#newsBtn").classList.add('visible')
         }
         document.querySelector(".news-slider").classList.add('d-none');
         document.querySelector(".pillar-slider").classList.remove('d-none');
@@ -20,7 +21,8 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("#pillarTab").classList.add('d-none');
         document.querySelector("#newsTab").classList.remove('d-none');
         document.querySelector("#newsTab").classList.add('d-block');
-        document.querySelector("#newsBtn").classList.add('d-none');
+        document.querySelector("#newsBtn").classList.add('invisible');
+        document.querySelector("#newsBtn").classList.remove('visible');
         document.querySelector(".pillar-slider").classList.add('d-none');
         document.querySelector(".news-slider").classList.remove('d-none');
         document.querySelector("#pillarCounter").classList.add('d-none');
