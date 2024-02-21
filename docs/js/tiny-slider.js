a8e8d148bff09d2abf98cbe264c0d5989e5e6c8c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-02-21 15:30:01 +0000

    Trying a new bit for the btns

--- docs/js/tiny-slider.js
+++ docs/js/tiny-slider.js
@@ -83,60 +83,78 @@ document.addEventListener("DOMContentLoaded", function() {
               activeSlide = info.displayIndex;
               pillarCurrent.textContent = info.displayIndex;
     
-              if (activeSlide >= 1 && activeSlide <= 5) {
-                  carousel.to(1);
-                  document.getElementById('esg-icon').classList.add('active');
-                  document.getElementById('tech-icon').classList.remove('active');
-                  document.getElementById('cust-icon').classList.remove('active');
-                  if(!document.querySelector('.esg-text').classList.contains('d-block')){
-                    document.querySelector('.esg-text').classList.add('d-block');
-                    document.querySelector('.esg-text').classList.remove('d-none');
+                if (activeSlide >= 1 && activeSlide <= 5) {
+                    carousel.to(1);
+                    document.getElementById('esg-icon').classList.add('active');
+                    document.getElementById('tech-icon').classList.remove('active');
+                    document.getElementById('cust-icon').classList.remove('active');
+                    if(!document.querySelector('.esg-text').classList.contains('d-block')){
+                        document.querySelector('.esg-text').classList.add('d-block');
+                        document.querySelector('.esg-text').classList.remove('d-none');
+                        document.querySelector('.esg-btn').classList.add('d-block');
+                        document.querySelector('.esg-btn').classList.remove('d-none');
                     }
                     if(!document.querySelector('.tech-text').classList.contains('d-none')){
                         document.querySelector('.tech-text').classList.add('d-none');
                         document.querySelector('.tech-text').classList.remove('d-block');
+                        document.querySelector('.tech-btn').classList.add('d-none');
+                        document.querySelector('.tech-btn').classList.remove('d-block');
                     }
                     if(!document.querySelector('.cust-text').classList.contains('d-none')){
                         document.querySelector('.cust-text').classList.add('d-none');
                         document.querySelector('.cust-text').classList.remove('d-block');
+                        document.querySelector('.cust-btn').classList.add('d-none');
+                        document.querySelector('.cust-btn').classList.remove('d-block');
                     }
-              }
+                }
     
-              else if (activeSlide >= 6 && activeSlide <= 10) {
-                  carousel.to(2);
-                  document.getElementById('esg-icon').classList.remove('active');
-                  document.getElementById('tech-icon').classList.add('active');
-                  document.getElementById('cust-icon').classList.remove('active');
-                  if(!document.querySelector('.tech-text').classList.contains('d-block')){
-                    document.querySelector('.tech-text').classList.add('d-block');
-                    document.querySelector('.tech-text').classList.remove('d-none');
+                else if (activeSlide >= 6 && activeSlide <= 10) {
+                    carousel.to(2);
+                    document.getElementById('esg-icon').classList.remove('active');
+                    document.getElementById('tech-icon').classList.add('active');
+                    document.getElementById('cust-icon').classList.remove('active');
+                    if(!document.querySelector('.tech-text').classList.contains('d-block')){
+                        document.querySelector('.tech-text').classList.add('d-block');
+                        document.querySelector('.tech-text').classList.remove('d-none');
+                        document.querySelector('.tech-btn').classList.add('d-block');
+                        document.querySelector('.tech-btn').classList.remove('d-none');
                     }
                     if(!document.querySelector('.esg-text').classList.contains('d-none')){
                         document.querySelector('.esg-text').classList.add('d-none');
                         document.querySelector('.esg-text').classList.remove('d-block');
+                        document.querySelector('.esg-btn').classList.add('d-none');
+                        document.querySelector('.esg-btn').classList.remove('d-block');
                     }
                     if(!document.querySelector('.cust-text').classList.contains('d-none')){
                         document.querySelector('.cust-text').classList.add('d-none');
                         document.querySelector('.cust-text').classList.remove('d-block');
+                        document.querySelector('.cust-btn').classList.add('d-none');
+                        document.querySelector('.cust-btn').classList.remove('d-block');
                     }
-              }
+                }
     
-              else if (activeSlide >= 11 && activeSlide <= 15) {
-                  carousel.to(3);
-                  document.getElementById('tech-icon').classList.remove('active');
-                  document.getElementById('esg-icon').classList.remove('active');
-                  document.getElementById('cust-icon').classList.add('active');
-                  if(!document.querySelector('.cust-text').classList.contains('d-block')){
-                    document.querySelector('.cust-text').classList.add('d-block');
-                    document.querySelector('.cust-text').classList.remove('d-none');
+                else if (activeSlide >= 11 && activeSlide <= 15) {
+                    carousel.to(3);
+                    document.getElementById('tech-icon').classList.remove('active');
+                    document.getElementById('esg-icon').classList.remove('active');
+                    document.getElementById('cust-icon').classList.add('active');
+                    if(!document.querySelector('.cust-text').classList.contains('d-block')){
+                        document.querySelector('.cust-text').classList.add('d-block');
+                        document.querySelector('.cust-text').classList.remove('d-none');
+                        document.querySelector('.cust-btn').classList.add('d-block');
+                        document.querySelector('.cust-btn').classList.remove('d-none');
                     }
                     if(!document.querySelector('.esg-text').classList.contains('d-none')){
                         document.querySelector('.esg-text').classList.add('d-none');
                         document.querySelector('.esg-text').classList.remove('d-block')
+                        document.querySelector('.esg-btn').classList.add('d-none');
+                        document.querySelector('.esg-btn').classList.remove('d-block');
                     }
                     if(!document.querySelector('.tech-text').classList.contains('d-none')){
                         document.querySelector('.tech-text').classList.add('d-none');
-                        document.querySelector('.tech-text').classList.remove('d-block')
+                        document.querySelector('.tech-text').classList.remove('d-block');
+                        document.querySelector('.tech-btn').classList.add('d-none');
+                        document.querySelector('.tech-btn').classList.remove('d-block');
                     }
               }
           });
